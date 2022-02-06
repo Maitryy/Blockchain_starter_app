6efbd6b47dfd5a1335ed62f12ce598f115035d6e
maitryy <maitryjadiya123@gmail.com>
2022-02-07 01:41:58 +0530

    first migration

--- defi-staking-app-starter/truffle-config.js
+++ defi-staking-app-starter/truffle-config.js
@@ -2,7 +2,7 @@ require('babel-register');
 require('babel-polyfill');
 
 module.exports = {
-    network:{
+    networks:{
         development:{
             host: "127.0.0.1",
             port: 7545,
