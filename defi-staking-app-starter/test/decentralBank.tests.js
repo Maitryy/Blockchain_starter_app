e38297ba0cffc12b6ce5211f875be13e3e7a051d
maitryy <maitryjadiya123@gmail.com>
2022-02-08 22:07:32 +0530

    mocha testing basics

---
+++ defi-staking-app-starter/test/decentralBank.tests.js
@@ -0,0 +1,62 @@
+// const { assert } = require('console');
+const assert = require('assert');
+const Tether = artifacts.require('Tether');
+const RWD = artifacts.require('RWD');
+const DecentralBank = artifacts.require('DecentralBank');
+
+require('chai')
+.use(require('chai-as-promised'))
+.should()
+
+contract('DecentralBank', ([owner, customer]) => {
+    let tether, rwd, decentralBank;
+
+    function tokens(number) {
+        return web3.utils.toWei(number, 'ether')
+    }
+
+    before(async () => {
+        tether = await Tether.new();
+        rwd = await RWD.new();
+        decentralBank = await DecentralBank.new(rwd.address, tether.address);
+        // all tokens to Dbank
+        await rwd.transfer(decentralBank.address, tokens('1000000') )
+
+        //transfer 100 tkns to customer
+
+        await tether.transfer(customer, tokens('100'), {from : owner})
+    })
+
+    describe('Mock Tether Deployment', async () => {
+        it('matches names successfully', async () => {
+            // let tether = await Tether.new();
+            //load contracts..
+            const name = await tether.name();
+            assert.equal(name, 'Mock Tether Token')
+        })
+    })
+
+    describe('Reward Token', async () => {
+        it('matches names successfully', async () => {
+            // let rwd = await RWD.new();
+            const name = await rwd.name();
+            assert.equal(name, 'Reward Token')
+        })
+    })
+
+    describe('Decentral Bank Deployment', async () => {
+        it('matches names successfully', async () => {
+            // let rwd = await RWD.new();
+            const name = await decentralBank.name();
+            assert.equal(name, 'Decentral Bank')
+        })
+
+        it('contract has tokens', async() => {
+            let balance = await rwd.balanceOf(decentralBank.address)
+            assert.equal(balance, tokens('1000000'))
+        })
+    })
+
+    
+    
+})
